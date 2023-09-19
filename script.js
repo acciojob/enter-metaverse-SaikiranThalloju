b24d3161d3de1c8f1ef74ea6d901f47c56cb0509
SaikiranThalloju <139635423+SaikiranThalloju@users.noreply.github.com>
2023-09-19 19:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 //your JS code here. If required.
-const button = document.getElementbyId("enterBtn");
-const para = document.getElementbyId("status");
-
-button.addEventListener("click", () => {
-	para.innerHTML = "<h1>Entered Metaverse</h1>";
-})
